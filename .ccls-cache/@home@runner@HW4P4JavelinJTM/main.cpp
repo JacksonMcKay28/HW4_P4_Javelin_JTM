dd3e2a4e9ca0e6a59e1c6fd700331b6d79e3bff6
JacksonMcKay28 <111894468+JacksonMcKay28@users.noreply.github.com>
2022-11-25 17:47:26 +0000

    sum before -1 sections

--- .ccls-cache/@home@runner@HW4P4JavelinJTM/main.cpp
+++ .ccls-cache/@home@runner@HW4P4JavelinJTM/main.cpp
@@ -3,8 +3,25 @@
 using namespace std;
 
 int main() {
-  
+  int * Lengths;
+  int n = 0;
+  cin >> n;
 
+  Lengths = new int[n];
+
+  for (int i = 0; i < n; i++) 
+  {
+    cin >> Lengths[i];
+  }
+
+  int sum = 0;
+
+  for (int j = 0; j < n; j++) 
+  {
+    sum = sum + Lengths[j];
+  }
+
+  cout << "Sum is " << sum;
 
   
 }
